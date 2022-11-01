caa5e8119900b9f7f2ad0743717d582f4ab15363
myodev00 <myodev00@gmail.com>
2022-11-01 07:17:57 +0700

    bug-fix/change-OldPhonePadService-to-interface
    change-OldPhonePadService-to-interface

---
+++ OldPhonePad/IServices/IOldPhonePadServices.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OldPhonePad.IServices
+{
+    public interface IOldPhonePadServices
+    {
+        bool ValidateInput(string keyInput);
+        string ConvertKeyAsAlphabetic(string keyInput);
+    }
+}
